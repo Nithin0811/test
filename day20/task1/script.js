c2f84656a79b2727b753f2d618eca39faa8f681d
Nithin0811 <nithinsankar0804@gmail.com>
2024-05-27 20:25:54 +0530

    day20

--- day20/task1/script.js
+++ day20/task1/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Fetch universities using Promise
+    // Fetch Bitcoin price using Promise
     const fetchData = () => {
         return new Promise((resolve, reject) => {
-            fetch('http://universities.hipolabs.com/search?country=United+States')
+            fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
                 .then(response => {
                     if (!response.ok) {
                         throw new Error('Failed to fetch data');
@@ -14,25 +14,53 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     };
 
-    // Display universities
-    const displayUniversities = (universities) => {
-        const universityList = document.getElementById('universityList');
-        if (universityList) {
-            universities.forEach(university => {
-                const listItem = document.createElement('li');
-                listItem.classList.add('list-group-item');
-                listItem.textContent = `Univeristy Name: ${university.name}, 
-                Univeristy Country: ${university.country}, 
-                Univeristy Alpha_Two_Code: ${university.alpha_two_code}, 
-                Univeristy Domain: ${university.domains}, 
-                Univeristy Website: ${university.web_pages}`;
-                universityList.appendChild(listItem);
-            });
-        }
+    // Display Bitcoin price
+    const displayPrice = (data) => {
+        const price = document.getElementById('price');
+        price.classList.add('list-group-item');
+        price.textContent = `
+            Rate: ${data.bpi.USD.rate}, 
+            Code: ${data.bpi.USD.code},
+            Symbols: ${data.bpi.USD.symbol},
+            Rate Float: ${data.bpi.USD.rate_float}
+            `;
+
+        const price1 = document.getElementById('price1');
+        price1.classList.add('list-group-item');  
+        price1.textContent = `
+            Rate: ${data.bpi.EUR.rate}, 
+            Code: ${data.bpi.EUR.code},
+            Symbols: ${data.bpi.EUR.symbol},
+            Rate Float: ${data.bpi.EUR.rate_float}
+            `;
+
+        const price2 = document.getElementById('price2');
+        price2.classList.add('list-group-item');  
+        price2.textContent = `
+            Rate: ${data.bpi.GBP.rate}, 
+            Code: ${data.bpi.GBP.code},
+            Symbols: ${data.bpi.GBP.symbol},
+            Rate Float: ${data.bpi.GBP.rate_float}
+            `;
+
+        const time = document.getElementById('time');
+        time.classList.add('list-group-item');  
+        time.textContent = `
+                Updated: ${data.time.updated}, 
+                UpdatedISO: ${data.time.updatedISO},
+                Updateduk: ${data.time.updateduk}
+                `;
+
+            price.appendChild(price);
+            price1.appendChild(price1);
+            price2.appendChild(price2);
+            time.appendChild(time);
+
+
     };
 
-    // Fetch data and display universities
+    // Fetch data and display price
     fetchData()
-        .then(data => displayUniversities(data))
+        .then(data => displayPrice(data))
         .catch(error => console.error('Error fetching data:', error));
 });
