f3ac25af58ae310902892d8ae780a05f1d1d50ab
Nithin0811 <nithinsankar0804@gmail.com>
2024-06-13 14:41:33 +0530

    resubmission

--- day4/anonymous.js
+++ day4/anonymous.js
@@ -1,150 +1,86 @@
-//print odd numbers in array
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-b = function() {
-let c = [];
-for (let i = 0; i < a.length; i++) {
-if (a[i] % 2 != 0) {
-c.push(a[i]);
-}
-}
-console.log(c);
-};
-b();
-(function() {
-let c = [];
-for (let i = 0; i < a.length; i++) {
-if (a[i] % 2 != 0) {
-c.push(a[i]);
-}
-}
-console.log(c);
-})();
-
-// Convert all the strings to title caps in a string array
-b = function(vj) {
-
-    var sentence = vj.toLowerCase().split();
-    
-    for (let i = 0; i < sentence.length; i++) {
-    
-    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-    
+//a)Print odd numbers in an array:
+(function (arr) {
+    for (let num of arr) {
+      if (num % 2 !== 0) {
+        console.log(num);
+      }
     }
-    
-    console.log(sentence.join());
-    
-    return sentence;
-    
-    }
-    b("i am the one super one");
-    (function(vj) {
-
-        var sentence = vj.toLowerCase().split();
-        
-        for (let i = 0; i < sentence.length; i++) {
-        
-        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-        
-        }
-        
-        console.log(sentence.join());
-        
-        return sentence;
-        
-        })("i am the one super one"); 
-
-//Sum of all numbers in an array
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-b = function() {
-let sum = 0;
-for (let i = 0; i < a.length; i++) {
-sum = sum + a[i];
-}
-return sum;
-}
-console.log(b());
-(function() {
-
-    let sum = 0;
-    
-    for (let i = 0; i < a.length; i++) {
-    
-    sum = sum + a[i];
-    
-    }
-    
+  })([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  
+  
+  //b)Convert all the strings to title caps in a string array
+  
+  (function (arr) {
+    let titleCaps = arr.map((str) => str.charAt(0).toUpperCase() + str.slice(1));
+    console.log(titleCaps);
+  })(["hello", "world", "javascript"]);
+  
+  
+  //c)Sum of all numbers in an array
+  (function (arr) {
+    let sum = arr.reduce((total, num) => total + num, 0);
     console.log(sum);
-    
-    })();
-
-
-//Return all the prime numbers in an array
-let p = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
-b = function(...p) {
-
-    for (let i = 0; i < p.length; i++) {
-    
-    for (let j = 2; j <= p[i]-1; j++) {
-    
-    if (p[i] % j == 0) {
-    
-    return false;
-    
+  })([1, 2, 3, 4, 5]);
+  
+  
+  //d)Return all the prime numbers in an array
+  (function (arr) {
+    function isPrime(num) {
+      if (num <= 1) return false;
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+      }
+      return true;
     }
-    
+  
+    let primeNums = arr.filter((num) => isPrime(num));
+    console.log(primeNums);
+  })([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  
+  
+  //e)Return all the palindromes in an array
+  (function (arr) {
+    function isPalindrome(str) {
+      let reversed = str.split("").reverse().join("");
+      return str === reversed;
     }
-    
-    return p[i] > 1;
-    
+  
+    let palindromes = arr.filter((str) => isPalindrome(str));
+    console.log(palindromes);
+  })(["hello", "level", "world", "racecar"]);
+  
+  
+  //f)Return median of two sorted arrays of the same size
+  (function (arr1, arr2) {
+    function findMedian(arr) {
+      let length = arr.length;
+      let mid = Math.floor(length / 2);
+      if (length % 2 === 0) {
+        return (arr[mid - 1] + arr[mid]) / 2;
+      } else {
+        return arr[mid];
+      }
     }
-    
-    };
-    
-    console.log(p.filter(b));
-    (prime = function(a) {
-
-        for (let j = 2; j <= p-1; j++) {
-        
-        if (p % j == 0) {
-        
-        return false;
-        
-        }
-        
-        }
-        
-        return p > 1
-        
-        }) ();
-        
-        console.log(p.filter(prime))
-
-//Remove duplicates from an array
-let arr1 = [1, 2, 2, 2, 4, 6, 6, 6, 7, 7, 8, 8, 8, 8, 8, 9, 22, 4, 4, 567, 67, 78, 78, 45, 67, 45, 45, 45, 78, 67, 56, 54];
-let dup = function(arr1) {
-    let newarr1 = [...new Set(arr1)];
-console.log(newarr1);
-}
-dup(arr1);
-(function(arr1) {
-let newarr1 = [...new Set(arr1)];
-console.log(newarr1);
-})(arr1)
-
-//Rotate an array by k times
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let k = 3;
-let rot = function(arr) {
-for (let i = 0; i < k; i++) {
-arr.unshift(arr.pop());
-}
-console.log(arr);
-}
-rot(arr);
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-(function(arr) {
-for (let i = 0; i < k; i++) {
-arr.unshift(arr.pop());
-}
-console.log(arr);
-})(arr);
+  
+    let merged = arr1.concat(arr2);
+    let sorted = merged.sort((a, b) => a - b);
+    let median = findMedian(sorted);
+    console.log(median);
+  })([1, 3, 5], [2, 4, 6]);
+  
+  
+  //g)Remove duplicates from an array
+  (function (arr) {
+    let uniqueArr = arr.filter(
+      (value, index, self) => self.indexOf(value) === index
+    );
+    console.log(uniqueArr);
+  })([1, 2, 3, 3, 4, 5, 5]);
+  
+  
+  //h)Rotate an array by k times
+  (function (arr, k) {
+    k = k % arr.length;
+    let rotated = arr.slice(k).concat(arr.slice(0, k));
+    console.log(rotated);
+  })([1, 2, 3, 4, 5], 2);
